95f575f93a417d5279e6be852754b01d4275fa56
r0227n <developryo@gmail.com>
2022-07-28 08:27:49 +0900

    add: Add a FileDirectoryNotifier. FileDirectoryNotifier notifies of file changes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:io' show Process;
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart' show launchUrl;
@@ -32,12 +32,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-final workDirectoryFilePathProvider = StateProvider<List<String>>((ref) {
-  final env = ref.watch(envProvider);
-  final lsResult = Process.runSync('ls', [env.workDirecotryPath]);
-
-  return lsResult.stdout.toString().split('\n');
-});
+final _currentFile = Provider<File>((ref) => throw UnimplementedError());
 
 class MyHomePage extends HookConsumerWidget {
   const MyHomePage({super.key, required this.title});
@@ -46,27 +41,107 @@ class MyHomePage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final files = ref.watch(workDirectoryFilePathProvider.state).state;
+    final files = ref.watch(fileDirectoryProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
       body: ListView.builder(
-        itemCount: files.length - 1,
+        itemCount: files.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(files[index]),
-            onTap: () {
-              // TODO: URL作成処理をきれいにする
-              launchUrl(Uri.parse(
-                      'file:///${ref.read(envProvider).workDirecotryPath}/${files[index]}'))
-                  .then(print)
-                  .catchError(print);
-            },
+          return ProviderScope(
+            overrides: [
+              _currentFile.overrideWithValue(files[index]),
+            ],
+            child: const CurrentFile(),
           );
         },
       ),
     );
+  }
+}
+
+class CurrentFile extends HookConsumerWidget {
+  const CurrentFile({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final file = ref.watch(_currentFile);
+
+    return ListTile(
+      title: Text(file.path),
+      onTap: () {
+        print(file.path);
+        // TODO: URL作成処理をきれいにする
+        launchUrl(Uri.parse('file:///${file.path}'))
+            .then(print)
+            .catchError(print);
+      },
+    );
+  }
+}
+
+/// Provide a [FileDirectoryNotifier]
+final fileDirectoryProvider =
+    StateNotifierProvider.autoDispose<FileDirectoryNotifier, List<File>>((ref) {
+  final directory = ref.watch(envProvider).workDirecotry;
+
+  // Retrieves files in the specified directory
+  List<File> files = directory
+      .listSync(recursive: true, followLinks: false)
+      .where((e) => e.path.contains(FileDirectoryNotifier.ignoreFile) == false)
+      .map((e) => File(e.path))
+      .toList()
+    ..sortLastModifired();
+
+  return FileDirectoryNotifier(files);
+});
+
+/// Retrieves and manages files in a specified directory.
+/// Handles events when there is a change in the specified directory.
+class FileDirectoryNotifier extends StateNotifier<List<File>> {
+  FileDirectoryNotifier([List<File>? files]) : super(files ?? const []) {
+    _watchDirectory();
+  }
+
+  /// Specify file to ignore with [FileDirectoryNotifier] & [fileDirectoryProvider]
+  static const String ignoreFile = '.DS_Store';
+
+  /// Watching a Directory for Changes
+  /// An event is fired when a file is created, renamed, or deleted in the specified directory.
+  /// Add to state: Creating or Renaming file
+  /// Delete to state: Deleting or Renaming file
+  void _watchDirectory() {
+    state.first.parent.watch(recursive: false).listen((entity) {
+      // Skip processing if the file to be ignored has changed
+      if (entity.path.contains(ignoreFile)) {
+        return;
+      }
+
+      switch (entity.runtimeType) {
+        case FileSystemCreateEvent:
+          // Adding from the list when creating or renaming file
+          state = [
+            ...state,
+            File(entity.path),
+          ]..sortLastModifired();
+          break;
+        case FileSystemDeleteEvent:
+          // Deleteing from the list when deleting or renaming file
+          state = state.where((file) => file.path != entity.path).toList();
+          break;
+        default:
+          throw Exception('Unknown event type: ${entity.runtimeType}');
+      }
+    });
+  }
+}
+
+/// [List<File>] extension
+extension FileDirectoryExtension on List<File> {
+  /// Sort by edit date in asc order.
+  void sortLastModifired() {
+    sort((a, b) => b.lastModifiedSync().compareTo(a.lastModifiedSync()));
   }
 }
