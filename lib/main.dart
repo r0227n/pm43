be9b65310bfb49036f03cd546228f89974638daf
r0227n <developryo@gmail.com>
2022-07-30 09:04:34 +0900

    edit: Execute file acquisition process without constructor

--- lib/main.dart
+++ lib/main.dart
@@ -88,40 +88,39 @@ final fileDirectoryProvider =
     StateNotifierProvider.autoDispose<FileDirectoryNotifier, List<File>>((ref) {
   final directory = ref.watch(envProvider).workDirecotry;
 
-  // Retrieves files in the specified directory
-  List<File> files = directory
-      .listSync(recursive: true, followLinks: false)
-      .where((e) => FileDirectoryNotifier.monitoreExtensions
-          .contains(p.context.extension(e.path)))
-      .map((e) => File(e.path))
-      .toList()
-    ..sortLastModifired();
-
-  return FileDirectoryNotifier(directory, files);
+  return FileDirectoryNotifier(directory);
 });
 
 /// Retrieves and manages files in a specified directory.
 /// Handles events when there is a change in the specified directory.
 class FileDirectoryNotifier extends StateNotifier<List<File>> {
-  FileDirectoryNotifier(this.direcotry, [List<File>? files]) : super(files ?? const []) {
+  FileDirectoryNotifier(this.directory) : super(const []) {
+    // Retrieves files in the specified directory
+    state =  directory
+      .listSync(recursive: true, followLinks: false)
+      .where((e) => FileDirectoryNotifier.monitoreExtensions.contains(p.context.extension(e.path)))
+      .map((e) => File(e.path))
+      .toList()
+    ..sortLastModifired();
+
     _watchDirectory();
   }
 
   /// The directory to watch.
-  final Directory direcotry;
+  final Directory directory;
 
   /// Files to be monitore with [FileDirectoryNotifier] & [fileDirectoryProvider]
   static const List<String> monitoreExtensions = ['.webm', '.mp3', '.mp4'];
 
   /// Get the directory name.
-  String get currentName => direcotry.name;
+  String get currentName => directory.name;
 
   /// Watching a Directory for Changes
   /// An event is fired when a file is created, renamed, or deleted in the specified directory.
   /// Add to state: Creating or Renaming file
   /// Delete to state: Deleting or Renaming file
   void _watchDirectory() {
-    direcotry.watch(recursive: false).listen((entity) {
+    directory.watch(recursive: false).listen((entity) {
       // // Skip files other than those to be monitore.
       if (monitoreExtensions.contains(entity.path)) {
         return;
