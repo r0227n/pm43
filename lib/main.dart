44d79fa057ad56b7a4b3fc006b6988eb81eaf6cb
Ryo24 <40818362+r0227n@users.noreply.github.com>
2022-08-02 07:32:30 +0900

    Watch a file directory (#2)
    * add: file_picker package
    
    * edit: Sandbox value of macos from true to false
    
    * add: list a file & segue player
    
    * remove: remove a file_picker package
    
    * add: Load setting.json program & Create Env class
    
    * add: .metadata
    
    * edit: Edit to read json files independent of flutter.
    
    * Delete .metadata
    
    * add: add a hooks_riverpod package
    
    * add: Load a setting.json & Application use a enviroment variables
    
    * add: Add a FileDirectoryNotifier. FileDirectoryNotifier notifies of file changes.
    
    * edit: changed to monitor specific files.
    
    * add: get the directory name
    
    * edit: Execute file acquisition process without constructor

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,31 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:url_launcher/url_launcher.dart' show launchUrl;
+import 'package:path/path.dart' as p;
+import 'env.dart';
 
 void main() {
-  runApp(const MyApp());
+  Env.initialize().then((env) {
+    runApp(
+      ProviderScope(
+        overrides: [
+          envProvider.overrideWithValue(env),
+        ],
+        child: const MyApp(),
+      ),
+    );
+  }).catchError(print);
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -29,87 +33,130 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+final _currentFile = Provider<File>((ref) => throw UnimplementedError());
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class MyHomePage extends HookConsumerWidget {
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final files = ref.watch(fileDirectoryProvider);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: files.length,
+        itemBuilder: (context, index) {
+          return ProviderScope(
+            overrides: [
+              _currentFile.overrideWithValue(files[index]),
+            ],
+            child: const CurrentFile(),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class CurrentFile extends HookConsumerWidget {
+  const CurrentFile({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final file = ref.watch(_currentFile);
+
+    return ListTile(
+      title: Text(file.path),
+      onTap: () {
+        print(file.path);
+        // TODO: URL作成処理をきれいにする
+        launchUrl(Uri.parse('file:///${file.path}'))
+            .then(print)
+            .catchError(print);
+      },
+    );
+  }
+}
+
+/// Provide a [FileDirectoryNotifier]
+final fileDirectoryProvider =
+    StateNotifierProvider.autoDispose<FileDirectoryNotifier, List<File>>((ref) {
+  final directory = ref.watch(envProvider).workDirecotry;
+
+  return FileDirectoryNotifier(directory);
+});
+
+/// Retrieves and manages files in a specified directory.
+/// Handles events when there is a change in the specified directory.
+class FileDirectoryNotifier extends StateNotifier<List<File>> {
+  FileDirectoryNotifier(this.directory) : super(const []) {
+    // Retrieves files in the specified directory
+    state =  directory
+      .listSync(recursive: true, followLinks: false)
+      .where((e) => FileDirectoryNotifier.monitoreExtensions.contains(p.context.extension(e.path)))
+      .map((e) => File(e.path))
+      .toList()
+    ..sortLastModifired();
+
+    _watchDirectory();
+  }
+
+  /// The directory to watch.
+  final Directory directory;
+
+  /// Files to be monitore with [FileDirectoryNotifier] & [fileDirectoryProvider]
+  static const List<String> monitoreExtensions = ['.webm', '.mp3', '.mp4'];
+
+  /// Get the directory name.
+  String get currentName => directory.name;
+
+  /// Watching a Directory for Changes
+  /// An event is fired when a file is created, renamed, or deleted in the specified directory.
+  /// Add to state: Creating or Renaming file
+  /// Delete to state: Deleting or Renaming file
+  void _watchDirectory() {
+    directory.watch(recursive: false).listen((entity) {
+      // // Skip files other than those to be monitore.
+      if (monitoreExtensions.contains(entity.path)) {
+        return;
+      }
+
+      switch (entity.runtimeType) {
+        case FileSystemCreateEvent:
+          // Adding from the list when creating or renaming file
+          state = [
+            ...state,
+            File(entity.path),
+          ]..sortLastModifired();
+          break;
+        case FileSystemDeleteEvent:
+          // Deleteing from the list when deleting or renaming file
+          state = state.where((file) => file.path != entity.path).toList();
+          break;
+        default:
+          print(entity);
+          break;
+        // throw Exception('Unknown event type: ${entity.runtimeType}');
+      }
+    });
+  }
+}
+
+/// [List<File>] extension
+extension FileDirectoryExtension on List<File> {
+  /// Sort by edit date in asc order.
+  void sortLastModifired() {
+    sort((a, b) => b.lastModifiedSync().compareTo(a.lastModifiedSync()));
+  }
+}
+
+/// [Directory] extension
+extension DirectoryExtension on Directory {
+  /// Get the directory name.
+  String get name => p.split(path).last;
 }
