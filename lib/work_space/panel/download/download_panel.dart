4d91d6f390faeb36ab8fcf9814ce40fc2c0ffdc2
r0227n <developryo@gmail.com>
2022-08-06 09:01:57 +0900

    add: add a SupportedExtension enum

--- lib/work_space/panel/download/download_panel.dart
+++ lib/work_space/panel/download/download_panel.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 class DownloadPanel extends StatelessWidget {
-  const DownloadPanel({Key? key}) : super(key: key);
+  const DownloadPanel({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Material(
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              autofocus: true,
+              decoration: const InputDecoration(
+                labelText: 'URL',
+                 prefixIcon: Icon(Icons.help_outline),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
