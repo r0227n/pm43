7d4a6673dfc7854070ca13cb761a04fc7237cb20
r0227n <developryo@gmail.com>
2022-08-07 00:33:22 +0900

    add: Supported extension chip view

--- lib/work_space/panel/download/download_panel.dart
+++ lib/work_space/panel/download/download_panel.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart' show HookWidget, useValueNotifier;
+import 'components/wrap_filter_chip.dart';
+import '../../util/supported_extension.dart';
 
-class DownloadPanel extends StatelessWidget {
+/// WorkSpace Download Panel
+class DownloadPanel extends HookWidget {
   const DownloadPanel({super.key});
 
   @override
   Widget build(BuildContext context) {
+    /// [_FilterChipWidget] State
+    final chipState = useValueNotifier(List<bool>.filled(
+        SupportedExtension.values.length, false,
+        growable: false));
+
     return Material(
-      child: Form(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             TextFormField(
               autofocus: true,
               decoration: const InputDecoration(
                 labelText: 'URL',
-                 prefixIcon: Icon(Icons.help_outline),
               ),
             ),
+            const Spacer(),
+            const Text('Encode Type'),
+            const Divider(),
+            WrapFilterChip(
+              chipState,
+              SupportedExtension.values.map((e) => e.name).toList(),
+            ),
+            const Spacer(),
+            Center(
+              child: ElevatedButton(
+                onPressed: () => print(chipState),
+                child: const Text('hoge'),
+              ),
+            ),
+            const Spacer(),
           ],
         ),
       ),
