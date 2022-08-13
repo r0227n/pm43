ac33b17dba2d40153f6da6ffbaade6cdbe71a304
r0227n <developryo@gmail.com>
2022-08-13 21:49:47 +0900

    add: Get a youtube-dl progress

--- lib/exec/youtube-dl/youtube_dl_command.dart
+++ lib/exec/youtube-dl/youtube_dl_command.dart
@@ -1,19 +1,17 @@
 import 'dart:io' show Process;
-import 'dart:async' show Completer;
 import 'dart:convert' show utf8;
+import 'dart:async' show Completer, StreamController;
 import '../exec.dart';
 import '../../work_space/util/video_format.dart';
 
 /// Wrap youTube-dl command
 class YoutubeDlCommand implements Exec {
   YoutubeDlCommand() {
-    Process.run(command, [help])
-      .then((result) {
-        if (result.exitCode != 0) {
-          throw Exception('youtube-dl error status ${result.exitCode}: ${result.stderr}');
-        }
-      })
-      .catchError((e) => throw Exception(e));
+    Process.run(command, [help]).then((result) {
+      if (result.exitCode != 0) {
+        throw Exception('youtube-dl error status ${result.exitCode}: ${result.stderr}');
+      }
+    }).catchError((e) => throw Exception(e));
   }
 
   @override
@@ -21,6 +19,33 @@ class YoutubeDlCommand implements Exec {
 
   @override
   final String help = '--help';
+  
+  /// Get youtube-dl progress
+  Stream<double> get progress => _progressController.stream;
+
+  /// Controller of [progress]
+  /// control or youtube-dl command progress 
+  final _progressController = StreamController<double>();
+
+  /// Parse youtube-dl command standard output
+  double _parseStdout(String stdout) {
+    if (!stdout.contains('[download]')) {
+      return 0.0;
+    }
+
+    // Get a percentage from the standard output content
+    final percentage = stdout.split(' ').firstWhere((separate) => separate.contains('%'));
+
+    // Delete the symbol and make double value
+    final double? num = double.tryParse(percentage.substring(0, percentage.length - 1));
+
+    if (num == null) {
+      throw Exception('youtube-dl standard output parse error: $percentage');
+    }
+
+    return num;
+  }
+
 
   /// Execute youtube-dl command
   /// arguments: command options
@@ -33,17 +58,14 @@ class YoutubeDlCommand implements Exec {
     /// Get the output of the command
     process.stdout
       .transform(utf8.decoder)
-      .forEach(print) // TODO: % を取得し、double値で外部から閲覧できるようにする
+      .forEach((String output) => _progressController.add(_parseStdout(output)))
       .whenComplete(() async => completer.complete(await process.exitCode))
       .catchError((e) => throw Exception(e));
 
     /// Error handling
     /// If an error occurs even once during command execution, the process is terminated.
-    process.stderr
-      .transform(utf8.decoder)
-      .first
-      .then((error) => throw Exception(error));
-    
+    process.stderr.first.then((error) => throw Exception(error));
+
     return completer.future;
   }
 
