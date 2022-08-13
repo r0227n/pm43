592bca90a2703aa069cc75fd27587411f34995d8
r0227n <developryo@gmail.com>
2022-08-13 20:07:55 +0900

    refactor: Change the way youtube-dl exec

--- lib/exec/youtube-dl/youtube_dl_command.dart
+++ lib/exec/youtube-dl/youtube_dl_command.dart
@@ -1,13 +1,19 @@
+import 'dart:io' show Process;
 import 'dart:async' show Completer;
-import 'dart:io' show Process, ProcessResult;
+import 'dart:convert' show utf8;
 import '../exec.dart';
 import '../../work_space/util/video_format.dart';
 
+/// Wrap youTube-dl command
 class YoutubeDlCommand implements Exec {
   YoutubeDlCommand() {
     Process.run(command, [help])
-        .then((validateProcess))
-        .catchError((e) => throw Exception(e));
+      .then((result) {
+        if (result.exitCode != 0) {
+          throw Exception('youtube-dl error status ${result.exitCode}: ${result.stderr}');
+        }
+      })
+      .catchError((e) => throw Exception(e));
   }
 
   @override
@@ -16,45 +22,52 @@ class YoutubeDlCommand implements Exec {
   @override
   final String help = '--help';
 
+  /// Execute youtube-dl command
+  /// arguments: command options
+  /// return: Exit code
   @override
-  Future<ProcessResult> run(List<String> arguments) {
-    final completer = Completer<ProcessResult>();
-    Process.run(command, arguments).then((process) {
-      validateProcess(process);
-      completer.complete(process);
-    }).catchError((e) => throw Exception(e));
+  Future<int> exec(List<String> arguments) async {
+    final completer = Completer<int>();
+    final process = await Process.start(command, arguments);
 
+    /// Get the output of the command
+    process.stdout
+      .transform(utf8.decoder)
+      .forEach(print) // TODO: % を取得し、double値で外部から閲覧できるようにする
+      .whenComplete(() async => completer.complete(await process.exitCode))
+      .catchError((e) => throw Exception(e));
+
+    /// Error handling
+    /// If an error occurs even once during command execution, the process is terminated.
+    process.stderr
+      .transform(utf8.decoder)
+      .first
+      .then((error) => throw Exception(error));
+    
     return completer.future;
   }
 
-  @override
-  void validateProcess(ProcessResult result) {
-    if (result.exitCode != 0) {
-      throw Exception('youtube-dl error status ${result.exitCode}: ${result.stderr}');
-    }
-  }
-
-  Future<ProcessResult> download(VideoFormat format, String outputPath, String videoId) {
+  Future<int> download(VideoFormat format, String outputPath, String videoId) {
     switch (format) {
       case VideoFormat.mp3:
-        /// Download youtube videos as mp3
-        return run([
+        // Download youtube videos as mp3
+        return exec([
           '--extract-audio',
           '--audio-format',
           format.name,
           '--embed-thumbnail',
           '--add-metadata',
           '--output',
-          '"$outputPath"',
+          '$outputPath/%(title)s.%(ext)s',
           videoId
         ]);
       case VideoFormat.mp4:
-        /// Download youtube videos as mp4
-        return run([
+        // Download youtube videos as mp4
+        return exec([
           '-f',
-          format.name, 
+          format.name,
           '--output',
-          '"$outputPath"',
+          '$outputPath/%(title)s.%(ext)s',
           videoId
         ]);
     }
