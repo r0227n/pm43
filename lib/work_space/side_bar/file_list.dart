87ae6f3388f17c77df56cc6304fbe0ca6e4749e0
Ryo24 <40818362+r0227n@users.noreply.github.com>
2022-08-14 10:20:08 +0900

    File ListView (#6)
    * edit: Only file names are displayed
    
    * add: ListView foloder

--- lib/work_space/side_bar/file_list.dart
+++ lib/work_space/side_bar/file_list.dart
@@ -1,23 +1,31 @@
 import 'dart:io' show File;
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart' show launchUrl;
+import 'package:path/path.dart' as p show basename;
 import 'file_directory_provider.dart';
 
 final _currentFile = Provider<File>((ref) => throw UnimplementedError());
 
 class FileList extends HookConsumerWidget {
-  const FileList({super.key});
+  const FileList({super.key, this.panel,});
+
+  final bool? panel;
+  
+  bool get _display => panel ?? false;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final files = ref.watch(fileDirectoryProvider);
+    final scroll = useScrollController();
 
     return Scaffold(
-      appBar: AppBar(
+      appBar: _display ? null :  AppBar(
         title: Text(ref.read(fileDirectoryProvider.notifier).currentName),
       ),
-      body: ListView.builder(
+      body: ListView.separated(
+        controller: scroll,
         itemCount: files.length,
         itemBuilder: (context, index) {
           return ProviderScope(
@@ -26,6 +34,9 @@ class FileList extends HookConsumerWidget {
             ],
             child: const CurrentFile(),
           );
+        },
+        separatorBuilder: (BuildContext context, int index) {  
+          return const Divider(height: 0.5);
         },
       ),
     );
@@ -40,7 +51,7 @@ class CurrentFile extends HookConsumerWidget {
     final file = ref.watch(_currentFile);
 
     return ListTile(
-      title: Text(file.path),
+      title: Text(p.basename(file.path)),
       onTap: () {
         print(file.path);
         // TODO: URL作成処理をきれいにする
