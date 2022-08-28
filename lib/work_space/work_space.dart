70539540eb48f1628d50b9719e34eddd4e9e1cb9
r0227n <developryo@gmail.com>
2022-08-29 07:43:17 +0900

    add: AlertDialog setting

--- lib/work_space/work_space.dart
+++ lib/work_space/work_space.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:flutter_hooks/flutter_hooks.dart' show useState;
+import 'package:flutter_hooks/flutter_hooks.dart' show useState, useTextEditingController;
+import 'package:pm43/data/local/local_data_provider.dart';
 import 'util/navigation_icon.dart';
 import 'activity_bar/activity_bar.dart';
 import 'panel/panel.dart';
+import 'panel/download/widgets/input_text_field.dart';
 
 class WorkSpace extends HookConsumerWidget {
   const WorkSpace({super.key, required this.title});
@@ -13,17 +15,56 @@ class WorkSpace extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final _selectedIndex = useState<NavigationIcon>(NavigationIcon.folder);
+    final _txtController = useTextEditingController();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  insetPadding: EdgeInsets.zero,
+                  title: const Text('Setting Workdirectory'),
+                  content: FittedBox(
+                      fit: BoxFit.cover,
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        child: InputTextField(
+                          _txtController,
+                          ref.read(localStorageProvider.notifier).workerDirecotryPath ?? 'null',
+                          focus: true,
+                        ),
+                      )),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'Cancel'),
+                      child: const Text('Cancel'),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'OK'),
+                      child: const Text('OK'),
+                    ),
+                  ],
+                ),
+              );
+            },
+          )
+        ],
       ),
       body: Row(
         children: <Widget>[
-          ActivityBar(_selectedIndex,),
+          ActivityBar(
+            _selectedIndex,
+          ),
           Expanded(
             flex: 8,
-            child: Panel(_selectedIndex,),
+            child: Panel(
+              _selectedIndex,
+            ),
           ),
         ],
       ),
